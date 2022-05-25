4c69795f4a2f22da3bf9c23405cfee89744e2dc9
rishonkumar <rishonkumarrahi@gmail.com>
2022-05-25 08:18:18 +0530

    check

---
+++ Check/check.java
@@ -0,0 +1,9 @@
+package Check;
+
+public class check {
+        public static void main(String[] args) {
+            System.out.println("Hello");
+            System.out.println("RIshon kumar");
+        }
+
+}
