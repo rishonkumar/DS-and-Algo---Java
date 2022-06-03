ac0ddf3f35984c18ec873d4e08458811d31de572
rishonkumar <rishonkumarrahi@gmail.com>
2022-06-03 18:17:55 +0530

    Graphs

--- Graphs/Graphs.java
+++ Graphs/Graphs.java
@@ -5,8 +5,22 @@ import java.util.Scanner;
 public class Graphs {
 
 
-    
-
+    public static void dfTraversal(int[][] adjMatrix, int currentVertex,boolean[] visited) {
+        visited[currentVertex] = true;
+        //print current Vertex
+        System.out.print(currentVertex + " ");
+        //figure out neigbors of currentVertex
+        //whichever cells have 1 then it is negibor
+        for(int i=0 ; i<adjMatrix.length ; i++) {
+            if(adjMatrix[currentVertex][i] == 1 && visited[i] == false) {
+                dfTraversal(adjMatrix,i,visited);
+            }
+        }
+    }
+    public static void dfTraversal(int[][] adjMatrix) {
+        boolean[] visited = new boolean[adjMatrix.length];
+        dfTraversal(adjMatrix,0,visited);
+    }
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
@@ -23,15 +37,15 @@ public class Graphs {
             adjMatrix[v1][v2] = 1;
             adjMatrix[v2][v1] = 1;
         }
-
+        dfTraversal(adjMatrix);
 
         //printing
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<n;j++) {
-                System.out.print(adjMatrix[i][j] + " ");
-            }
-            System.out.println();
-        }
+        // for(int i=0;i<n;i++) {
+        //     for(int j=0;j<n;j++) {
+        //         System.out.print(adjMatrix[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
 
     }
 }
